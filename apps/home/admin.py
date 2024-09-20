b0dfc2b6f3f4af211fe3815166f5a8a79d95d123
Priyanshu Singh <sbhola833@gmail.com>
2024-09-21 02:44:33 +0530

    add models and create superuser

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Category, Item, Nutrition
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)  
+
+class NutritionInline(admin.StackedInline):  
+    model = Nutrition 
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'price', 'status')
+    list_filter = ('category', 'status')
+    inlines = [NutritionInline]  
+
+@admin.register(Nutrition)
+class NutritionAdmin(admin.ModelAdmin):
+    list_display = ('item_id', 'item_name', 'calories', 'fat', 'protein') 
+
+    def item_id(self, obj):
+        return obj.item.id
+    item_id.short_description = 'Item ID'
+
+    def item_name(self, obj):
+        return obj.item.name
+    item_name.short_description = 'Item Name' 
--- apps/home/models.py
+++ apps/home/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    last_modified = models.DateTimeField(auto_now=True)  
+
+    def __str__(self):
+        return self.name
+    
+
+    def save(self, *args, **kwargs):
+        for field in self._meta.fields:
+            if field.get_internal_type() == 'CharField':
+                value = getattr(self, field.name)
+                if isinstance(value, str):
+                    setattr(self, field.name, value.lower())
+        
+        super().save(*args, **kwargs)    
+    
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1, related_name='items')
+    price = models.DecimalField(max_digits=6, decimal_places=2) 
+
+    class ItemStatus(models.TextChoices):
+        AVAILABLE = 'available', 'Available'
+        NOT_AVAILABLE = 'not_available', 'Not Available'
+
+    status = models.CharField(
+        max_length=20,
+        choices=ItemStatus.choices,
+        default=ItemStatus.NOT_AVAILABLE
+    )
+
+    description = models.TextField(blank=True) 
+    last_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+    def save(self, *args, **kwargs):
+        for field in self._meta.fields:
+            if field.get_internal_type() == 'CharField' or 'TextField':
+                value = getattr(self, field.name)
+                if isinstance(value, str):
+                    setattr(self, field.name, value.lower())
+        
+        super().save(*args, **kwargs)
+
+class Nutrition(models.Model):
+    item = models.OneToOneField(Item, on_delete=models.CASCADE, related_name='nutrition')
+    calories = models.IntegerField(default=0)
+    fat = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+    protein = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+    carbohydrates = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+    sugar = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+    last_modified = models.DateTimeField(auto_now=True)  
+
+    def __str__(self):
+        return f"Nutrition for {self.item.name}"
